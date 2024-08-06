8804cb5b4d05a683af254026bdd45ce64641f88c
Gauri-dev-ops <maligayatri083@gmail.com>
2024-08-06 09:22:39 +0530

    Add files via upload
    urban

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', () => {
+  const navLinks = document.querySelectorAll('nav ul li a');
+  navLinks.forEach(link => {
+      link.addEventListener('click', (e) => {
+          e.preventDefault();
+          const targetId = link.getAttribute('href').substring(1);
+          const targetSection = document.getElementById(targetId);
+          window.scrollTo({
+              top: targetSection.offsetTop,
+              behavior: 'smooth'
+          });
+      });
+  });
+});
